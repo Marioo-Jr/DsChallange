dfe4eb3627c1c1719881a953d5b6dc6566b2634d
Marioo-Jr <mario.junior23@univille.br>
2025-08-11 00:14:27 -0300

    ecommerce

--- Exercicios/ecommerce/src/main/java/com/ecommerceJava/ecommerce/entities/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.ecommerceJava.ecommerce.entities;
-
-public enum OrderStatus {
-    WAITING_PAYMENT,PAID, SHIPPED, DELIVERED, CANCELLED;
-}
--- Exercicios/ecommerce/src/main/java/com/ecommerceJava/ecommerce/services/exeptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerceJava.ecommerce.services.exeptions;
-
-public class ResourceNotFoundException extends RuntimeException { // usar o RuntimeExeption nao precisa usar Try Catch
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-
-}
