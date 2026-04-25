1f27e20de3e2c6ab1ab10aa44d11ef921c0fee59
Marioo-Jr <mario.junior23@univille.br>
2026-04-25 16:29:17 -0300

    BCrypt PassWord

--- Exercicios/spring-boot-oauth2-jwt-demo/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ Exercicios/spring-boot-oauth2-jwt-demo/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$1qFGeinwvwfzxW/upO6tV.pwwqRkKYQsXiljQRxkGLh5UQwVZMsR6');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$1qFGeinwvwfzxW/upO6tV.pwwqRkKYQsXiljQRxkGLh5UQwVZMsR6');
 
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
